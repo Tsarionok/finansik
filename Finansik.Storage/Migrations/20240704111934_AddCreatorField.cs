83e152564e55719dcf3a934e5416638530d433bb
Sergey Tsarionok <tsarionok@gmail.com>
2024-07-04 14:42:07 +0300

    add missing migration

---
+++ Finansik.Storage/Migrations/20240704111934_AddCreatorField.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finansik.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatorField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Groups",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Creator",
+                table: "Groups",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Creator",
+                table: "Categories",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Groups",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
