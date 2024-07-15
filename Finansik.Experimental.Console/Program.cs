164d7fd85bff8bb090e2fbdee6661a8f8c7a5156
Sergei Tsarionok <tsarionok@gmail.com>
2024-07-15 18:55:22 +0300

    multithreading experiment

--- Finansik.Experimental.Console/Program.cs
+++ Finansik.Experimental.Console/Program.cs
@@ -1,24 +1,72 @@
-﻿var ok = ThreadPool.SetMinThreads(40, 20);
+﻿var _lock = new object();
 
-var task1 = Task.Run(async () =>
+var common = 1;
+
+var iterator = new ConcurrentIterator();
+
+var threadOne = new Thread(Print);
+threadOne.Name = "ONE";
+var threadTwo = new Thread(Print);
+threadTwo.Name = "TWO";
+Thread.Sleep(300);
+
+threadOne.Start();
+threadTwo.Start();
+
+while (threadOne.IsAlive && threadTwo.IsAlive) { }
+
+Console.WriteLine($"Total executions: {iterator.Value}");
+
+void Print()
 {
-    await Task.Delay(8000);
-    Console.WriteLine("FIRST RUN");
-});
+    while (common <= 200)
+    {
+        var isLocked = false;
+        try
+        {
+            Monitor.Enter(_lock, ref isLocked);
+            for (var i = 0; i <= 5; i++)
+            {
+                if (common > 200)
+                    break;
 
-var task2 = Task.Run(async () =>
+                if (common == 42)
+                    throw new Exception();
+        
+                Console.WriteLine($"[{Thread.CurrentThread.Name}] {common++}");
+                iterator.Increment();
+            }
+        }
+        finally
+        {
+            if (isLocked)
+            {
+                Monitor.Exit(_lock);
+                Console.WriteLine($"[{Thread.CurrentThread.Name}] if finally");
+            }
+            else
+            {
+                Console.WriteLine($"[{Thread.CurrentThread.Name}] else finally");
+            }
+        }
+            
+        Thread.Sleep(10);
+        Console.WriteLine($"[{Thread.CurrentThread.Name}] continue");
+    }
+}
+
+
+public class ConcurrentIterator
 {
-    await Task.Delay(5000);
-    Console.WriteLine("SECOND RUN");
-});
+    private readonly object _lock = new();
 
+    public int Value { get; private set; }
 
-Thread.CurrentThread.Name = "FinansikThread";
-
-Console.WriteLine(Thread.CurrentThread.Name);
-
-Task.WaitAll(task1, task2);
-
-
-
-
+    public void Increment()
+    {
+        lock (_lock)
+        {
+            ++Value;
+        }
+    }
+}
