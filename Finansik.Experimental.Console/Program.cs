9c2d4ecb9772586d338d74f4a60627239744ecb5
Sergei Tsarionok <tsarionok@gmail.com>
2024-07-15 10:38:53 +0300

    task experiment

--- Finansik.Experimental.Console/Program.cs
+++ Finansik.Experimental.Console/Program.cs
@@ -1,4 +1,24 @@
-﻿Console.WriteLine("");
+﻿var ok = ThreadPool.SetMinThreads(40, 20);
+
+var task1 = Task.Run(async () =>
+{
+    await Task.Delay(8000);
+    Console.WriteLine("FIRST RUN");
+});
+
+var task2 = Task.Run(async () =>
+{
+    await Task.Delay(5000);
+    Console.WriteLine("SECOND RUN");
+});
+
+
+Thread.CurrentThread.Name = "FinansikThread";
+
+Console.WriteLine(Thread.CurrentThread.Name);
+
+Task.WaitAll(task1, task2);
+
 
 
 
