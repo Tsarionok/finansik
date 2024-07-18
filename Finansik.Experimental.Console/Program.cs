5d5b18a007e083e9f4432af32edabc03625e256c
Sergei Tsarionok <tsarionok@gmail.com>
2024-07-18 15:40:28 +0300

    experimental

--- Finansik.Experimental.Console/Program.cs
+++ Finansik.Experimental.Console/Program.cs
@@ -1,72 +1,31 @@
-﻿var _lock = new object();
+﻿FuncManager.Execute();
 
-var common = 1;
+await Task.Delay(1000);
 
-var iterator = new ConcurrentIterator();
 
-var threadOne = new Thread(Print);
-threadOne.Name = "ONE";
-var threadTwo = new Thread(Print);
-threadTwo.Name = "TWO";
-Thread.Sleep(300);
-
-threadOne.Start();
-threadTwo.Start();
-
-while (threadOne.IsAlive && threadTwo.IsAlive) { }
-
-Console.WriteLine($"Total executions: {iterator.Value}");
-
-void Print()
+public class FuncManager
 {
-    while (common <= 200)
+    public static void Execute()
     {
-        var isLocked = false;
         try
         {
-            Monitor.Enter(_lock, ref isLocked);
-            for (var i = 0; i <= 5; i++)
+            var text = "Hello";
+            Console.WriteLine("Start");
+            Func(async () =>
             {
-                if (common > 200)
-                    break;
-
-                if (common == 42)
-                    throw new Exception();
-        
-                Console.WriteLine($"[{Thread.CurrentThread.Name}] {common++}");
-                iterator.Increment();
-            }
+                Console.WriteLine(text);
+                throw new NotImplementedException();
+            });
+            Console.WriteLine("End");
         }
-        finally
+        catch (Exception e)
         {
-            if (isLocked)
-            {
-                Monitor.Exit(_lock);
-                Console.WriteLine($"[{Thread.CurrentThread.Name}] if finally");
-            }
-            else
-            {
-                Console.WriteLine($"[{Thread.CurrentThread.Name}] else finally");
-            }
+            Console.WriteLine("Exception handled");
         }
-            
-        Thread.Sleep(10);
-        Console.WriteLine($"[{Thread.CurrentThread.Name}] continue");
     }
-}
-
-
-public class ConcurrentIterator
-{
-    private readonly object _lock = new();
-
-    public int Value { get; private set; }
-
-    public void Increment()
+    
+    public static void Func(Action action)
     {
-        lock (_lock)
-        {
-            ++Value;
-        }
+        action();
     }
 }
