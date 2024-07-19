4463dc8c64bf8244a531c2f57c00cdf400db6e82
Sergei Tsarionok <tsarionok@gmail.com>
2024-07-19 15:39:43 +0300

    add storage tests project

---
+++ Finansik.Storage.Tests/StorageTestFixture.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using AutoMapper;
+using Finansik.Storage.Entities.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Testcontainers.PostgreSql;
+
+namespace Finansik.Storage.Tests;
+
+public class StorageTestFixture : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder().Build();
+
+    public FinansikDbContext GetDbContext() => new(new DbContextOptionsBuilder<FinansikDbContext>()
+        .UseNpgsql(_dbContainer.GetConnectionString()).Options);
+
+    public IMapper GetMapper() =>
+        new Mapper(new MapperConfiguration(cfg =>
+            cfg.AddMaps(Assembly.GetAssembly(typeof(FinansikDbContext)))));
+
+    public IMemoryCache GetMemoryCache() => new MemoryCache(new MemoryCacheOptions());
+    
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        var dbContext = new FinansikDbContext(new DbContextOptionsBuilder<FinansikDbContext>()
+            .UseNpgsql(_dbContainer.GetConnectionString()).Options);
+        await dbContext.Database.MigrateAsync();
+    }
+
+    public async Task DisposeAsync() => await _dbContainer.DisposeAsync();
+}
---
+++ Finansik.Storage.Tests/Storages/CreateCategoryStorageShould.cs
@@ -0,0 +1,44 @@
+using System.Security.Cryptography;
+using AutoMapper;
+using Finansik.Storage.Entities;
+using Finansik.Storage.Storages;
+using FluentAssertions;
+
+namespace Finansik.Storage.Tests.Storages;
+
+public class CreateCategoryStorageShould : IClassFixture<StorageTestFixture>
+{
+    private readonly CreateCategoryStorage _sut;
+    private readonly FinansikDbContext _dbContext;
+
+    public CreateCategoryStorageShould(StorageTestFixture fixture)
+    {
+        _sut = new CreateCategoryStorage(fixture.GetDbContext(), new GuidFactory(), fixture.GetMapper());
+        _dbContext = fixture.GetDbContext();
+    }
+    
+    [Fact]
+    public async Task InsertNewCategoryInDatabase()
+    {
+        var groupId = Guid.Parse("4E8B58A2-89DB-4775-9D1A-4886E6812A24");
+        var userId = Guid.Parse("8F994B1D-A586-473D-BD98-5A9B61635F6A");
+        await _dbContext.Groups.AddAsync(new Group { Id = groupId, Name = "Testers"});
+        await _dbContext.Users.AddAsync(new User
+        {
+            Id = userId, 
+            Login = "admin", 
+            PasswordHash = RandomNumberGenerator.GetBytes(32),
+            Salt = RandomNumberGenerator.GetBytes(128)
+        });
+        await _dbContext.SaveChangesAsync();
+        
+        var category = await _sut.CreateCategory(
+            "Transport",
+            groupId,
+            userId,
+            "icon.png",
+            CancellationToken.None);
+
+        category.Id.Should().NotBeEmpty();
+    }
+}
