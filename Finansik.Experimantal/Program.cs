710b31c9e894d32550551e79965879ba2564d3ef
Sergei Tsarionok <tsarionok@gmail.com>
2024-07-22 14:31:56 +0300

    add experimental project

---
+++ Finansik.Experimantal/Program.cs
@@ -0,0 +1,33 @@
+﻿Person.Execute();
+
+internal record Person
+{
+    public string Name { get; set; }
+
+    public static void Execute()
+    {
+        Person person = new Stuff("ST", 334);
+
+        Type type = person.GetType();
+
+        type.Assembly.GetType();
+
+        Console.WriteLine(type.BaseType.Name);
+    }
+}
+
+internal record Stuff : Person
+{
+    public Stuff(string name, int salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+    
+    public int Salary { get; set; }
+}
+
+internal class Car
+{
+    public string Model { get; set; }
+}
